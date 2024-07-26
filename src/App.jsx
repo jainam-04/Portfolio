1ea9bc377933369d88c6759b9d2e2bffd6258f84
Jainam <jainamrupani04@gmail.com>
2024-07-26 20:11:06 +0530

    Skills section is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import Skills from './components/Skills'
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Skills />
     </div>
   )
 }
---
+++ src/components/Skills.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+function Skills() {
+      return (
+            <div>
+                  <div className="bg-cyan-950 skills">
+                        <h1 className='text-3xl text-center text-white mt-0 mb-10 pt-3'>Skills</h1>
+                        <div className='text-white text-lg pb-3'>
+                              <ul className='flex justify-center gap-20 items-center my-5 list-disc'>
+                                    <li>HTML</li>
+                                    <li>CSS</li>
+                                    <li>JavaScript</li>
+                              </ul>
+                              <ul className='flex justify-center gap-20 items-center my-5 list-disc'>
+                                    <li>React.js</li>
+                                    <li>TailWind CSS</li>
+                              </ul>
+                              <ul className='flex justify-center gap-20 items-center my-5 list-disc'>
+                                    <li>Java</li>
+                                    <li>AWT</li>
+                                    <li>Swing</li>
+                                    <li>JDBC</li>
+                              </ul>
+                              <ul className='flex justify-center gap-20 items-center my-5 list-disc'>
+                                    <li>SQL</li>
+                                    <li>MySQL</li>
+                                    <li>DBMS</li>
+                              </ul>
+                              <ul className='flex justify-center gap-20 items-center my-5 list-disc'>
+                                    <li>Git</li>
+                                    <li>GitHub</li>
+                                    <li>VsCode</li>
+                                    <li>IntelliJ Idea</li>
+                              </ul>
+                        </div>
+                  </div>
+            </div>
+      )
+}
+
+export default Skills
