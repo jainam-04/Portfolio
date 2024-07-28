6c86a630fc9c86ab9510b0324c01c7b574bfeb30
Jainam <jainamrupani04@gmail.com>
2024-07-28 16:00:13 +0530

    Applied react-router-dom to all files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import Skills from './components/Skills'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <Skills />
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route exact path='/skills' element={<Skills />}></Route>
+        </Routes>
+      </Router>
     </div>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
       return (
             <div>
                   <div className="flex justify-around items-center bg-blue-800 p-6">
                         <div>
-                              <h1 className='text-2xl text-white'>Jainam Rupani</h1>
+                              <h1 className='text-2xl text-white'><Link to="/">Jainam Rupani</Link></h1>
                         </div>
                         <div>
                               <ul className='flex gap-20 items-center text-lg text-white'>
                                     <li>
-                                          <a href="/">Home</a>
+                                          <Link to="/">Home</Link>
                                     </li>
                                     <li>
-                                          <a href="/skills">Skills</a>
+                                          <Link to="/skills">Skills</Link>
                                     </li>
                                     <li>
-                                          <a href="/projects">Projects</a>
+                                          <Link to="/projects">Projects</Link>
                                     </li>
                                     <li>
-                                          <a href="/contact">Contact</a>
+                                          <Link to="/contact">Contact</Link>
                                     </li>
                               </ul>
                         </div>
